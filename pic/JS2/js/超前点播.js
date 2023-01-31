97cce458daf66713d557d0d669d622e3ad98638d
l1007 <95461996+l1007@users.noreply.github.com>
2023-01-31 11:30:04 +0800

    Add files via upload

---
+++ pic/JS2/js/超前点播.js
@@ -0,0 +1,305 @@
+var rule = {
+    title:'超前点播',
+    host:'https://bzhanyy.com',
+    // homeUrl:'/',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:1,//是否启用分类筛选,
+	filter_url:'/{{fl.area}}/{{fl.by}}/{{fl.class}}/{{fl.cateId}}/{{fl.page}}/{{fl.year}}',
+	filter: {"1":[
+	{
+"key":"class",
+"name":"分类",
+"value":[
+{"n":"全部","v":""},
+{"n":"Netflix","v":"class/Netflix"},
+{"n":"喜剧","v":"class/喜剧"},
+{"n":"爱情","v":"class/爱情"},
+{"n":"恐怖","v":"class/恐怖"},
+{"n":"丧尸","v":"class/丧尸"},
+{"n":"动作","v":"class/动作"},
+{"n":"科幻","v":"class/科幻"},
+{"n":"剧情","v":"class/剧情"},
+{"n":"战争","v":"class/战争"},
+{"n":"灾难","v":"class/灾难"},
+{"n":"犯罪","v":"class/犯罪"},
+{"n":"动画","v":"class/动画"},
+{"n":"奇幻","v":"class/奇幻"},
+{"n":"冒险","v":"class/冒险"},
+{"n":"悬疑","v":"class/悬疑"},
+{"n":"惊悚","v":"class/惊悚"},
+{"n":"纪录","v":"class/纪录"},
+{"n":"网络电影","v":"class/网络电影"}
+]
+},
+{
+"key":"area",
+"name":"地区",
+"value":[
+{"n":"全部","v":""},
+{"n":"中国","v":"area/中国大陆"},
+{"n":"香港","v":"area/香港"},
+{"n":"台湾","v":"area/台湾"},
+{"n":"美国","v":"area/美国"},
+{"n":"日本","v":"area/日本"},
+{"n":"新加坡","v":"area/新加坡"},
+{"n":"马来西亚","v":"area/马来西亚"},
+{"n":"印度","v":"area/印度"},
+{"n":"韩国","v":"area/韩国"},
+{"n":"泰国","v":"area/泰国"},
+{"n":"其他","v":"area/其他"}
+]
+},
+{
+"key":"year",
+"name":"年份",
+"value":[
+{"n":"全部","v":""},
+{"n":"2023","v":"year/2023"},
+{"n":"2022","v":"year/2022"},
+{"n":"2021","v":"year/2021"},
+{"n":"2020","v":"year/2020"},
+{"n":"2019","v":"year/2019"},
+{"n":"2018","v":"year/2018"},
+{"n":"2017","v":"year/2017"},
+{"n":"2016","v":"year/2016"},
+{"n":"2015","v":"year/2015"},
+{"n":"2014","v":"year/2014"},
+{"n":"2013","v":"year/2013"},
+{"n":"2012","v":"year/2012"},
+{"n":"2011","v":"year/2011"},
+{"n":"2010","v":"year/2010"}
+]
+},
+{
+"key":"by",
+"name":"排序",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"2":[
+{
+"key":"class",
+"name":"分类",
+"value":[
+{"n":"全部","v":""},
+{"n":"Netflix","v":"class/Netflix"},
+{"n":"古装","v":"class/古装"},
+{"n":"战争","v":"class/战争"},
+{"n":"喜剧","v":"class/喜剧"},
+{"n":"丧尸","v":"class/丧尸"},
+{"n":"家庭","v":"class/家庭"},
+{"n":"犯罪","v":"class/犯罪"},
+{"n":"动作","v":"class/动作"},
+{"n":"奇幻","v":"class/奇幻"},
+{"n":"剧情","v":"class/剧情"},
+{"n":"科幻","v":"class/科幻"},
+{"n":"惊悚","v":"class/惊悚"},
+{"n":"恐怖","v":"class/恐怖"},
+{"n":"悬疑","v":"class/悬疑"},
+{"n":"穿越","v":"class/穿越"},
+{"n":"其他","v":"class/其他"}
+]
+},
+{
+"key":"area",
+"name":"地区",
+"value":[
+{"n":"全部","v":""},
+{"n":"中国","v":"area/内地"},
+{"n":"中国大陆","v":"area/中国大陆"},
+{"n":"香港","v":"area/香港"},
+{"n":"台湾","v":"area/台湾"},
+{"n":"美国","v":"area/美国"},
+{"n":"日本","v":"area/日本"},
+{"n":"新加坡","v":"area/新加坡"},
+{"n":"韩国","v":"area/韩国"},
+{"n":"泰国","v":"area/泰国"},
+{"n":"印度","v":"area/印度"},
+{"n":"其他","v":"area/其他"}
+]
+},
+{
+"key":"year",
+"name":"年份",
+"value":[
+{"n":"全部","v":""},
+{"n":"2023","v":"year/2023"},
+{"n":"2022","v":"year/2022"},
+{"n":"2021","v":"year/2021"},
+{"n":"2020","v":"year/2020"},
+{"n":"2019","v":"year/2019"},
+{"n":"2018","v":"year/2018"},
+{"n":"2017","v":"year/2017"},
+{"n":"2016","v":"year/2016"},
+{"n":"2015","v":"year/2015"},
+{"n":"2014","v":"year/2014"},
+{"n":"2013","v":"year/2013"},
+{"n":"2012","v":"year/2012"},
+{"n":"2011","v":"year/2011"},
+{"n":"2010","v":"year/2010"},
+{"n":"2009","v":"year/2009"},
+{"n":"2008","v":"year/2008"},
+{"n":"2007","v":"year/2007"},
+{"n":"2006","v":"year/2006"},
+{"n":"2005","v":"year/2005"},
+{"n":"2004","v":"year/2004"}
+]
+},
+{
+"key":"by",
+"name":"排序",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"4":[
+{
+"key":"area",
+"name":"地区",
+"value":[
+{"n":"全部","v":""},
+{"n":"中国","v":"area/内地"},
+{"n":"日本","v":"area/日本"},
+{"n":"美国","v":"area/美国"},
+{"n":"其他","v":"area/其他"}
+]
+},
+{
+"key":"year",
+"name":"年份",
+"value":[
+{"n":"全部","v":""},
+{"n":"2023","v":"year/2023"},
+{"n":"2022","v":"year/2022"},
+{"n":"2021","v":"year/2021"},
+{"n":"2020","v":"year/2020"},
+{"n":"2019","v":"year/2019"},
+{"n":"2018","v":"year/2018"},
+{"n":"2017","v":"year/2017"},
+{"n":"2016","v":"year/2016"},
+{"n":"2015","v":"year/2015"},
+{"n":"2014","v":"year/2014"},
+{"n":"2013","v":"year/2013"},
+{"n":"2012","v":"year/2012"},
+{"n":"2011","v":"year/2011"},
+{"n":"2010","v":"year/2010"},
+{"n":"2009","v":"year/2009"},
+{"n":"2008","v":"year/2008"},
+{"n":"2007","v":"year/2007"},
+{"n":"2006","v":"year/2006"},
+{"n":"2005","v":"year/2005"},
+{"n":"2004","v":"year/2004"}
+]
+},
+{
+"key":"by",
+"name":"排序",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+],
+"3":[
+{
+"key":"class",
+"name":"分类",
+"value":[
+{"n":"全部","v":""},
+{"n":"真人秀","v":"class/真人秀"},
+{"n":"游戏","v":"class/游戏"},
+{"n":"亲子","v":"class/亲子"},
+{"n":"音乐","v":"class/音乐"},
+{"n":"搞笑","v":"class/搞笑"},
+{"n":"少儿","v":"class/少儿"},
+{"n":"纪录","v":"class/纪录"},
+{"n":"其他","v":"class/其他"}
+]
+},
+{
+"key":"area",
+"name":"地区",
+"value":[
+{"n":"全部","v":""},
+{"n":"大陆","v":"area/内地"},
+{"n":"香港","v":"area/香港"},
+{"n":"韩国","v":"area/韩国"},
+{"n":"日本","v":"area/日本"},
+{"n":"台湾","v":"area/台湾"},
+{"n":"美国","v":"area/美国"}
+]
+},
+{
+"key":"year",
+"name":"年份",
+"value":[
+{"n":"全部","v":""},
+{"n":"2023","v":"year/2023"},
+{"n":"2022","v":"year/2022"},
+{"n":"2021","v":"year/2021"},
+{"n":"2020","v":"year/2020"},
+{"n":"2019","v":"year/2019"},
+{"n":"2018","v":"year/2018"},
+{"n":"2017","v":"year/2017"},
+{"n":"2016","v":"year/2016"},
+{"n":"2015","v":"year/2015"},
+{"n":"2014","v":"year/2014"},
+{"n":"2013","v":"year/2013"},
+{"n":"2012","v":"year/2012"},
+{"n":"2011","v":"year/2011"},
+{"n":"2010","v":"year/2010"},
+{"n":"2009","v":"year/2009"},
+{"n":"2008","v":"year/2008"},
+{"n":"2007","v":"year/2007"},
+{"n":"2006","v":"year/2006"},
+{"n":"2005","v":"year/2005"},
+{"n":"2004","v":"year/2004"}
+]
+},
+{
+"key":"by",
+"name":"排序",
+"value":[
+{"n":"全部","v":""},
+{"n":"时间","v":"time"},
+{"n":"人气","v":"hits"},
+{"n":"评分","v":"score"}
+]
+}
+]
+},
+	filter_def:{
+		1:{cateId:'1'},
+		2:{cateId:'2'},
+		3:{cateId:'3'},
+		4:{cateId:'4'}
+	},
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'1&2&3&4',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'.movie-list-body;.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.movie-list-body&&.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.cr3&&Text","img":".poster&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.scroll-content&&Text;.title-block&&.starLink&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text","content":".detailsTxt&&Text","tabs":".swiper-wrapper&&a","lists":".play_list_box:eq(#id)&&.content_playlist li"},
+    搜索:'.movie-list-body&&.vod-search-list;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+}
