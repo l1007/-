9dece4cda0a03a626209e7c470d9e9a1ad98a3b0
l1007 <95461996+l1007@users.noreply.github.com>
2023-09-29 14:16:57 +0800

    Update 虎牙直播.js

--- pic/JS2/js/虎牙直播.js
+++ pic/JS2/js/虎牙直播.js
@@ -4,8 +4,8 @@ var rule = {
     host:'https://www.huya.com',
     homeUrl:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=2168&tagAll=0&page=1',//用于"分类获取"和"推荐获取"
     url:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=fyfilter&tagAll=0&page=fypage',
-    class_name:'娱乐&网游&单机&手游',
-    class_url:'8&1&2&3',
+    class_name:'娱乐',
+    class_url:'8',
     detailUrl:'https://m.huya.com/fyid',//二级详情拼接链接(json格式用)
     filterable: 1,
     filter_url: '{{fl.area}}',
