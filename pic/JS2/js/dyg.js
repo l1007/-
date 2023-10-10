0ebb1b4eec7c579df5b29bd4f17fc5f71b764cdd
l1007 <95461996+l1007@users.noreply.github.com>
2023-10-10 11:50:26 +0800

    Update dyg.js

--- pic/JS2/js/dyg.js
+++ pic/JS2/js/dyg.js
@@ -1,212 +1,61 @@
 var rule = {
-	title:'电影港[磁]',
-	编码:'gb2312',
-	搜索编码:'gb2312',
-	host:'https://www.dygang.tv',
+	title:'磁力熊[磁]',
+	host:'https://www.cilixiong.com',
 	homeUrl:'/',
-	url: '/fyclass/index_fypage.htm?',
-	filter_url:'{{fl.class}}',
+	url: '/fyclassfyfilter-(fypage-1).html',
+	//host:'http://127.0.0.1:10079',
+	//homeUrl:'/p/0/socks5%253A%252F%252F192.168.101.1%253A1080/https://www.cilixiong.com',
+	//url:'/p/0/socks5%253A%252F%252F192.168.101.1%253A1080/https://www.cilixiong.com/fyclassfyfilter-(fypage-1).html',
+	filter_url:'-{{fl.class or "0"}}-{{fl.area or "0"}}',
 	filter:{
+		"1":[{"key":"class","name":"类型","value":[{"n":"全部","v":"0"},{"n":"剧情","v":"1"},{"n":"喜剧","v":"2"},{"n":"惊悚","v":"3"},{"n":"动作","v":"4"},{"n":"爱情","v":"5"},{"n":"犯罪","v":"6"},{"n":"恐怖","v":"7"},{"n":"冒险","v":"8"},{"n":"悬疑","v":"9"},{"n":"科幻","v":"10"},{"n":"家庭","v":"11"},{"n":"奇幻","v":"12"},{"n":"动画","v":"13"},{"n":"战争","v":"14"},{"n":"历史","v":"15"},{"n":"传记","v":"16"},{"n":"音乐","v":"17"},{"n":"歌舞","v":"18"},{"n":"运动","v":"19"},{"n":"西部","v":"20"},{"n":"灾难","v":"21"},{"n":"古装","v":"22"},{"n":"情色","v":"23"},{"n":"同性","v":"24"},{"n":"儿童","v":"25"},{"n":"纪录片","v":"26"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":"0"},{"n":"大陆","v":"1"},{"n":"香港","v":"2"},{"n":"台湾","v":"3"},{"n":"美国","v":"4"},{"n":"日本","v":"5"},{"n":"韩国","v":"6"},{"n":"英国","v":"7"},{"n":"法国","v":"8"},{"n":"德国","v":"9"},{"n":"印度","v":"10"},{"n":"泰国","v":"11"},{"n":"丹麦","v":"12"},{"n":"瑞典","v":"13"},{"n":"巴西","v":"14"},{"n":"加拿大","v":"15"},{"n":"俄罗斯","v":"16"},{"n":"意大利","v":"17"},{"n":"比利时","v":"18"},{"n":"爱尔兰","v":"19"},{"n":"西班牙","v":"20"},{"n":"澳大利亚","v":"21"},{"n":"波兰","v":"22"},{"n":"土耳其","v":"23"},{"n":"越南","v":"24"}]}],
+		"2":[{"key":"class","name":"类型","value":[{"n":"全部","v":"0"},{"n":"剧情","v":"1"},{"n":"喜剧","v":"2"},{"n":"惊悚","v":"3"},{"n":"动作","v":"4"},{"n":"爱情","v":"5"},{"n":"犯罪","v":"6"},{"n":"恐怖","v":"7"},{"n":"冒险","v":"8"},{"n":"悬疑","v":"9"},{"n":"科幻","v":"10"},{"n":"家庭","v":"11"},{"n":"奇幻","v":"12"},{"n":"动画","v":"13"},{"n":"战争","v":"14"},{"n":"历史","v":"15"},{"n":"传记","v":"16"},{"n":"音乐","v":"17"},{"n":"歌舞","v":"18"},{"n":"运动","v":"19"},{"n":"西部","v":"20"},{"n":"灾难","v":"21"},{"n":"古装","v":"22"},{"n":"情色","v":"23"},{"n":"同性","v":"24"},{"n":"儿童","v":"25"},{"n":"纪录片","v":"26"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":"0"},{"n":"大陆","v":"1"},{"n":"香港","v":"2"},{"n":"台湾","v":"3"},{"n":"美国","v":"4"},{"n":"日本","v":"5"},{"n":"韩国","v":"6"},{"n":"英国","v":"7"},{"n":"法国","v":"8"},{"n":"德国","v":"9"},{"n":"印度","v":"10"},{"n":"泰国","v":"11"},{"n":"丹麦","v":"12"},{"n":"瑞典","v":"13"},{"n":"巴西","v":"14"},{"n":"加拿大","v":"15"},{"n":"俄罗斯","v":"16"},{"n":"意大利","v":"17"},{"n":"比利时","v":"18"},{"n":"爱尔兰","v":"19"},{"n":"西班牙","v":"20"},{"n":"澳大利亚","v":"21"},{"n":"波兰","v":"22"},{"n":"土耳其","v":"23"},{"n":"越南","v":"24"}]}]
 	},
-	searchUrl: '/e/search/index123.php#tempid=1&tbname=article&keyborad=**&show=title%2Csmalltext&Submit=%CB%D1%CB%F7;post',
-	searchable:2,
+	searchUrl: '/p/0/socks5%253A%252F%252F192.168.101.1%253A1080/https://www.cilixiong.com/e/search/index.php#classid=1,2&show=title&tempid=1&keyboard=**;post',
+	searchable:0,
 	quickSearch:0,
-	filterable:0,
+	filterable:1,
 	headers:{
-		'User-Agent': 'MOBILE_UA',
-		'Referer': 'https://www.dygang.tv/'
+		'User-Agent': 'MOBILE_UA'
 	},
 	timeout:5000,
-	class_name:'最新电影&经典高清&国配电影&经典港片&国剧&日韩剧&美剧&综艺&动漫&纪录片&高清原盘&4K高清区&3D电影&电影专题',
-	class_url:'ys&bd&gy&gp&dsj&dsj1&yx&zy&dmq&jilupian&1080p&4K&3d&dyzt',
-	play_parse:true,
-	play_json:[{
-		re:'*',
-		json:{
-			parse:0,
-			jx:0
-		}
-	}],
+	class_name:'电影&剧集&豆瓣电影Top250&IMDB Top250&高分悬疑片&高分喜剧片&高分传记片&高分爱情片&高分犯罪片&高分恐怖片&高分冒险片&高分武侠片&高分奇幻片&高分历史片&高分战争片&高分歌舞片&高分灾难片&高分情色片&高分西部片&高分音乐片&高分科幻片&高分动作片&高分动画片&高分纪录片&冷门佳片',
+	class_url:'1&2&/top250/&/s/imdbtop250/&/s/suspense/&/s/comedy/&/s/biopic/&/s/romance/&/s/crime/&/s/horror/&/s/adventure/&/s/martial/&/s/fantasy/&/s/history/&/s/war/&/s/musical/&/s/disaster/&/s/erotic/&/s/west/&/s/music/&/s/sci-fi/&/s/action/&/s/animation/&/s/documentary/&/s/unpopular/',
+	play_parse:false,
 	lazy:'',
 	limit:6,
-	推荐:'div#tl tr:has(>td>table.border1>tbody>tr>td>a>img);table.border1 img&&alt;table.border1 img&&src;table:eq(2)&&Text;a&&href',
-	一级:`js:
+	推荐: `js:
 		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-		let d = [];
-		let turl = (MY_PAGE === 1)? '/' : '/index_'+ MY_PAGE + '.htm';
-		input = rule.homeUrl + MY_CATE + turl;
-		let html = request(input);
-		let list = pdfa(html, 'tr:has(>td>table.border1)');
+		var d = [];
+		var html = request(input);
+		var list = pdfa(html, 'body&&.col');
 		list.forEach(it => {
-			let title = pdfh(it, 'table.border1 img&&alt');
-			if (title!==""){
-				d.push({
-					title: title,
-					desc: pdfh(it, 'table:eq(1)&&Text'),
-					pic_url: pd(it, 'table.border1 img&&src', HOST),
-					url: pdfh(it, 'a&&href')
-				});
-			}
+			d.push({
+				title: pdfh(it, 'h2&&Text'),
+				desc: pdfh(it, '.me-auto&&Text') + '分 / ' + pdfh(it, '.small&&Text'),
+				pic_url: pd(it, '.card-img&&style')
+			});
 		})
 		setResult(d);
 	`,
-	二级:{
-		title:"div.title a&&Text",
-		img:"#dede_content img&&src",
-		desc:"#dede_content&&Text",
-		content:"#dede_content&&Text",
-		tabs:`js:
-pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-TABS=[]
-let d = pdfa(html, '#dede_content table tbody tr');
-let tabsa = [];
-let tabsq = [];
-let tabsm = false;
-let tabse = false;
-let tabm3u8 = [];
-d.forEach(function(it) {
-	let burl = pd(it, 'a&&href',HOST);
-	if (burl.startsWith("https://www.aliyundrive.com/s/")){
-		tabsa.push("阿里雲盤");
-	}else if (burl.startsWith("https://pan.quark.cn/s/")){
-		tabsq.push("夸克網盤");
-	}else if (burl.startsWith("magnet")){
-		tabsm = true;
-	}else if (burl.startsWith("ed2k")){
-		tabse = true;
-	}
-});
-if (false){
-d = pdfa(html, 'div:has(>div#post_content) div.widget:has(>h3)');
-d.forEach(function(it) {
-	tabm3u8.push(pdfh(it, 'h3&&Text'));
-});
-}
-if (tabsm === true){
-	TABS.push("磁力");
-}
-if (tabse === true){
-	TABS.push("電驢");
-}
-if (false && tabsa.length + tabsq.length > 1){
-	TABS.push("選擇右側綫路");
-}
-let tmpIndex;
-tmpIndex=1;
-tabsa.forEach(function(it){
-	TABS.push(it + tmpIndex);
-	tmpIndex = tmpIndex + 1;
-});
-tmpIndex=1;
-tabsq.forEach(function(it){
-	TABS.push(it + tmpIndex);
-	tmpIndex = tmpIndex + 1;
-});
-tabm3u8.forEach(function(it){
-	TABS.push(it);
-});
-log('dygang TABS >>>>>>>>>>>>>>>>>>' + TABS);
-`,
-		lists:`js:
-log(TABS);
-pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-LISTS = [];
-let d = pdfa(html, '#dede_content table tbody tr');
-let lista = [];
-let listq = [];
-let listm = [];
-let liste = [];
-let listm3u8 = {};
-d.forEach(function(it){
-	let burl = pd(it, 'a&&href',HOST);
-	let title = pdfh(it, 'a&&Text');
-	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
-	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
-	let loopresult = title + '$' + burl;
-	if (burl.startsWith("https://www.aliyundrive.com/s/")){
-		if (true){
-		if (TABS.length==1){
-			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
-		}else{
-			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+	一级: `js:
+		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+		var d = [];
+		if (MY_CATE !== '1' && MY_CATE !== '2') {
+			let turl = (MY_PAGE === 1)? 'index' : 'index_'+ MY_PAGE;
+			input = rule.homeUrl + MY_CATE + turl + '.html';
 		}
-		}else{
-			burl = 'push://' + burl;
-		}
-		loopresult = title + '$' + burl;
-		lista.push(loopresult);
-	}else if (burl.startsWith("https://pan.quark.cn/s/")){
-		if (true){
-		if (TABS.length==1){
-			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
-		}else{
-			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
-		}
-		}else{
-			burl = 'push://' + burl;
-		}
-		loopresult = title + '$' + burl;
-		listq.push(loopresult);
-	}else if (burl.startsWith("magnet")){
-		listm.push(loopresult);
-	}else if (burl.startsWith("ed2k")){
-		liste.push(loopresult);
-	}
-});
-if (listm.length>0){
-	LISTS.push(listm);
-}
-if (liste.length>0){
-	LISTS.push(liste);
-}
-lista.forEach(function(it){
-	LISTS.push([it]);
-});
-listq.forEach(function(it){
-	LISTS.push([it]);
-});
-for ( const key in listm3u8 ){
-	if (listm3u8.hasOwnProperty(key)){
-		LISTS.push(listm3u8[key]);
-	}
-};
-`,
-
-	},
-	搜索:`js:
-pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-let params = 'tempid=1&tbname=article&keyboard=' + KEY + '&show=title%2Csmalltext&Submit=%CB%D1%CB%F7';
-let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
-let postData = {
-    method: "POST",
-    body: params
-};
-delete(_fetch_params.headers['Content-Type']);
-Object.assign(_fetch_params, postData);
-log("dygang search postData>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
-let search_html = request( HOST + '/e/search/index123.php', _fetch_params, true);
-//log("dygang search result>>>>>>>>>>>>>>>" + search_html);
-let d=[];
-let dlist = pdfa(search_html, 'table.border1');
-dlist.forEach(function(it){
-	let title = pdfh(it, 'img&&alt');
-	if (searchObj.quick === true){
-		if (false && title.includes(KEY)){
-			title = KEY;
-		}
-	}
-	let img = pd(it, 'img&&src', HOST);
-	let content = pdfh(it, 'img&&alt');
-	let desc = pdfh(it, 'img&&alt');
-	let url = pd(it, 'a&&href', HOST);
-	d.push({
-		title:title,
-		img:img,
-		content:content,
-		desc:desc,
-		url:url
+		var html = request(input);
+		var list = pdfa(html, 'body&&.col');
+		list.forEach(it => {
+			d.push({
+				title: pdfh(it, 'h2&&Text'),
+				desc: pdfh(it, '.me-auto&&Text') + '分 / ' + pdfh(it, '.small&&Text'),
+				pic_url: pdfh(it, '.card-img&&style')
+			});
 		})
-});
-setResult(d);
-`,
+		setResult(d);
+	`,
+	二级:'',
+	搜索:'',
 }
