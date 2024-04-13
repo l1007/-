66f11a573df888240dc44ecb84f3ad9f3fe7bb5c
l1007 <95461996+l1007@users.noreply.github.com>
2024-04-13 11:58:25 +0800

    Update hw-e.js

--- pic/JS2/js/hw-e.js
+++ pic/JS2/js/hw-e.js
@@ -1,66 +1,178 @@
-var rule={
-	title:'海外',
-	// host:'https://olevod.live',
-	host:'https://olvod.vip',
-	homeUrl:'/index.php/label/rankweek.html', // 人气排行榜周榜
-	url:'/index.php/vod/show/id/fyfilter.html',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}{{fl.area}}{{fl.by or "/by/time"}}{{fl.class}}{{fl.lang}}{{fl.letter}}/page/fypage{{fl.year}}',
-	filter: {
-		"1":[{"key":"cateId","name":"class","value":[{"n":"All","v":"1"},{"n":"Adventure","v":"6"},{"n":"Feature","v":"7"},{"n":"Action","v":"8"},{"n":"Cartoon","v":"9"},{"n":"Comedy","v":"11"},{"n":"Fantasy","v":"12"},{"n":"Horror","v":"20"},{"n":"Suspense","v":"21"},{"n":"Thriller","v":"22"},{"n":"War","v":"23"},{"n":"Musical","v":"24"},{"n":"Disaster","v":"25"},{"n":"Affectional","v":"26"},{"n":"Criminal","v":"27"},{"n":"Science","v":"28"},{"n":"Classics","v":"29"},{"n":"Internet","v":"30"}]},{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"Hong Kong","v":"/area/香港"},{"n":"Taiwan","v":"/area/台湾"},{"n":"Us","v":"/area/欧美"},{"n":"Korea","v":"/area/韩国"},{"n":"Singapore Malaysia","v":"/area/新马"},{"n":"Thailand","v":"/area/泰国"},{"n":"India","v":"/area/印度"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"},{"n":"2003","v":"/year/2003"},{"n":"2002","v":"/year/2002"},{"n":"2001","v":"/year/2001"},{"n":"2000","v":"/year/2000"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}],
-		"2":[{"key":"cateId","name":"class","value":[{"n":"All","v":"2"},{"n":"US","v":"13"},{"n":"Japan","v":"14"},{"n":"Korea","v":"15"},{"n":"China","v":"16"},{"n":"Thailand","v":"31"},{"n":"Hong Kong","v":"32"},{"n":"Taiwan","v":"33"},{"n":"Singapore Malaysia","v":"34"}]},{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"Hong Kong","v":"/area/香港"},{"n":"Taiwan","v":"/area/台湾"},{"n":"Japan","v":"/area/日本"},{"n":"Korea","v":"/area/韩国"},{"n":"Us","v":"/area/欧美"},{"n":"Thailand","v":"/area/泰国"},{"n":"Singapore Malaysia","v":"/area/新马"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"},{"n":"2003","v":"/year/2003"},{"n":"2002","v":"/year/2002"},{"n":"2001","v":"/year/2001"},{"n":"2000","v":"/year/2000"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}],
-		"3":[{"key":"cateId","name":"class","value":[{"n":"All","v":"3"},{"n":"US","v":"36"},{"n":"Japan","v":"37"},{"n":"Korea","v":"38"},{"n":"China","v":"39"},{"n":"Singapore Malaysia","v":"40"},{"n":"Hong Kong","v":"41"}]},{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"Hong Kong","v":"/area/香港"},{"n":"Taiwan","v":"/area/台湾"},{"n":"Japan","v":"/area/日本"},{"n":"Korea","v":"/area/韩国"},{"n":"Us","v":"/area/欧美"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"},{"n":"2003","v":"/year/2003"},{"n":"2002","v":"/year/2002"},{"n":"2001","v":"/year/2001"},{"n":"2000","v":"/year/2000"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}],
-		"4":[{"key":"cateId","name":"class","value":[{"n":"All","v":"4"},{"n":"US","v":"43"},{"n":"Japan","v":"44"},{"n":"Korea","v":"45"},{"n":"China","v":"46"},{"n":"Singapore Malaysia","v":"47"},{"n":"Hong Kong","v":"48"}]},{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"Japan","v":"/area/日本"},{"n":"Korea","v":"/area/韩国"},{"n":"Us","v":"/area/欧美"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"},{"n":"2003","v":"/year/2003"},{"n":"2002","v":"/year/2002"},{"n":"2001","v":"/year/2001"},{"n":"2000","v":"/year/2000"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}],
-		"50":[{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"HK TW","v":"/area/港台"},{"n":"Us","v":"/area/欧美"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"},{"n":"2003","v":"/year/2003"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}],
-		"51":[{"key":"area","name":"Area","value":[{"n":"All","v":""},{"n":"China","v":"/area/大陆"},{"n":"Hong Kong","v":"/area/香港"},{"n":"Taiwan","v":"/area/台湾"},{"n":"Japan","v":"/area/日本"},{"n":"Korea","v":"/area/韩国"},{"n":"Us","v":"/area/欧美"}]},{"key":"year","name":"Year","value":[{"n":"All","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"},{"n":"2009","v":"/year/2009"},{"n":"2008","v":"/year/2008"},{"n":"2007","v":"/year/2007"},{"n":"2006","v":"/year/2006"},{"n":"2005","v":"/year/2005"},{"n":"2004","v":"/year/2004"}]},{"key":"lang","name":"Language","value":[{"n":"ALL","v":""},{"n":"Mandarin","v":"/lang/国语"},{"n":"English","v":"/lang/英语"},{"n":"Cantonese","v":"/lang/粤语"},{"n":"Korean","v":"/lang/韩语"},{"n":"Japanese","v":"/lang/日语"}]},{"key":"by","name":"By","value":[{"n":"Time","v":"/by/time"},{"n":"Hits","v":"/by/hits"},{"n":"Score","v":"/by/score"}]}]
+var rule = {
+	title:'MP4电影[磁]',
+	host:'https://www.mp4us.com',
+        homeUrl: '/',
+	url: '/list/fyclass-fypage.html?',
+	filter_url:'{{fl.class}}',
+	filter:{
 	},
-	filter_def:{
-		1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'},
-		50:{cateId:'50'},
-		51:{cateId:'51'}
-	},
-	searchable:2,//是否启用全局搜索,
-	quickSearch:0,//是否启用快速搜索,
+	searchUrl: '/search/**-1.html',
+	searchable:2,
+	quickSearch:0,
+	filterable:0,
 	headers:{
-		'User-Agent':'MOBILE_UA',
+		'User-Agent': 'PC_UA',
+         	'Cookie':''
 	},
-	class_name:'Movie&Drama&Tvshow&Comic&Documentary&Sport', // 分类筛选 /api.php/app/nav
-	class_url:'1&2&3&4&50&51',
-    play_parse:true,
-	lazy:`js:
-		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-		var url = html.url;
-		if (html.encrypt == "1") {
-			url = unescape(url)
-		} else if (html.encrypt == "2") {
-			url = unescape(base64Decode(url))
+	timeout:5000,
+	class_name: '动作片&科幻片&爱情片&喜剧片&恐怖片&战争片&剧情片&纪录片&动画片&电视剧',
+	class_url: '1&2&3&4&5&6&7&8&9&10',
+	play_parse:true,
+	play_json:[{
+		re:'*',
+		json:{
+			parse:0,
+			jx:0
 		}
-		if (/m3u8|mp4/.test(url)) {
-			input = url
-		} else {
-			input
-		}
-	`,
+	}],
+	lazy:'',
 	limit:6,
-	// double:true, // 推荐内容是否双层定位
-	// 推荐:'body&&.hl-list-wrap;ul&&li;*;*;*;*',
-	推荐:'.hl-rank-list&&.hl-list-item;*;*;.hl-item-hits&&Text;*',
-	一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+	推荐:'div.index_update ul li;a&&Text;;b&&Text;a&&href',
+	一级:'div#list_all ul li;img.lazy&&alt;img.lazy&&data-original;span.update_time&&Text;a&&href',
 	二级:{
-		"title":"h2&&Text;li.hl-col-xs-12--em:eq(6)&&Text",
-		"img":".hl-lazy&&data-original",
-		"desc":"li.hl-col-xs-12:eq(1)&&Text;;;li.hl-col-xs-12--em:eq(2)&&Text;li.hl-col-xs-12--em:eq(3)&&Text",
-		"content":".hl-content-text&&Text",
-		"tabs":".hl-from-list&&li",
-		"lists":".hl-plays-list:eq(#id) li"
-	},
+		title:"div.article-header h1&&Text",
+		img:"div.article-header div.pic img&&src",
+		desc:'div.article-header div.text&&Text',
+		content:'div.article-related.info p&&Text',
+		tabs:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+TABS=[]
+let d = pdfa(html, 'ul.down-list&&li a');
+let tabsa = [];
+let tabsq = [];
+let tabsm = false;
+let tabse = false;
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		tabsa.push("阿里雲盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克網盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
+	}
+});
+if (tabsm === true){
+	TABS.push("磁力");
+}
+if (tabse === true){
+	TABS.push("電驢");
+}
+if (false && tabsa.length + tabsq.length > 1){
+	TABS.push("選擇右側綫路");
+}
+let tmpIndex;
+tmpIndex=1;
+tabsa.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tmpIndex=1;
+tabsq.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+log('mp4us TABS >>>>>>>>>>>>>>>>>>' + TABS);
+`,
+		lists:`js:
+log(TABS);
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+LISTS = [];
+let d = pdfa(html, 'ul.down-list&&li a');
+let lista = [];
+let listq = [];
+let listm = [];
+let liste = [];
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('dygang title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('dygang burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+			burl = "push://" + burl;
+		}
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+                        burl = "push://" + burl;
+                }
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
+	}
+});
+if (listm.length>0){
+	LISTS.push(listm.reverse());
+}
+if (liste.length>0){
+	LISTS.push(liste.reverse());
+}
+if (false && lista.length + listq.length > 1){
+	LISTS.push(["選擇右側綫路，或3秒後自動跳過$http://127.0.0.1:10079/delay/"]);
+}
+lista.forEach(function(it){
+	LISTS.push([it]);
+});
+listq.forEach(function(it){
+	LISTS.push([it]);
+});
+`,
 
-	// searchUrl:'/index.php/vod/search.html?wd=**',
-	searchUrl:'/index.php/ajax/suggest?mid=1&wd=**&limit=50',
-	detailUrl:'/index.php/vod/detail/id/fyid.html', //非必填,二级详情拼接链接
-	// 搜索:'.hl-list-wrap&&.hl-item-pic;*;*;*;*',
-	搜索:'json:list;name;pic;;id',
+	},
+	搜索:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+if (rule_fetch_params.headers.Cookie.startsWith("http")){
+	rule_fetch_params.headers.Cookie=fetch(rule_fetch_params.headers.Cookie);
+	let cookie = rule_fetch_params.headers.Cookie;
+	setItem(RULE_CK, cookie);
+};
+log('mp4us seach cookie>>>>>>>>>>>>>' + rule_fetch_params.headers.Cookie);
+let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
+//log("mp4us search params>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
+let search_html = request( HOST + '/search/' + encodeURIComponent(KEY) + '-1.html', _fetch_params)
+//log("mp4us search result>>>>>>>>>>>>>>>" + search_html);
+let d=[];
+//'div#list_all li;img.lazy&&alt;img.lazy&&src;div.text_info h2&&Text;a&&href;p.info&&Text',
+let dlist = pdfa(search_html, 'div#list_all li');
+dlist.forEach(function(it){
+	let title = pdfh(it, 'img.lazy&&alt');
+	if (title.includes(KEY)){
+		if (searchObj.quick === true){
+			title = KEY;
+		}
+		let img = pd(it, 'img.lazy&&src', HOST);
+		let content = pdfh(it, 'div.text_info h2&&Text');
+		let desc = pdfh(it, 'p.info&&Text');
+		let url = pd(it, 'a&&href', HOST);
+		d.push({
+			title:title,
+			img:img,
+			content:content,
+			desc:desc,
+			url:url
+			})
+	}
+});
+setResult(d);
+`,
 }
