7a417e130b9d069f677e0caa6ed7878f1b6d5d4a
l1007 <95461996+l1007@users.noreply.github.com>
2024-04-13 12:06:15 +0800

    Update cl.js

--- pic/JS2/js/cl.js
+++ pic/JS2/js/cl.js
@@ -1,65 +1,242 @@
 var rule = {
-	title:'6V新版[磁]',
-	host:'http://www.xb6v.com/',
-	// host:'https://www.66s6.net',
-	// url: '/fyclass/index_fypage.html[/fyclass/index.html]',
-	url: '/fyclassfyfilter/index_fypage.html[/fyclassfyfilter/index.html]',
+	title:'新版6V[磁]',
+	host:'http://www.xb6v.com',
+	homeUrl:'/',
+	url: '/fyclass/index_fypage.html?',
 	filter_url:'{{fl.class}}',
 	filter:{
-		"dianshiju":[{"key":"class","name":"类型","value":[{"n":"全部","v":""},{"n":"国剧","v":"/guoju"},{"n":"日韩剧","v":"/rihanju"},{"n":"欧美剧","v":"/oumeiju"}]}]
 	},
-	searchUrl: '/e/search/index.php#show=title&tempid=1&tbname=article&mid=1&dopost=search&submit=&keyboard=**;post',
+	searchUrl: '/e/search/index.php#tempid=1&tbname=article&mid=1&dopost=search&submit=&keyborad=**;post',
 	searchable:2,
 	quickSearch:0,
-	filterable:1,
+	filterable:0,
 	headers:{
-		'User-Agent': 'MOBILE_UA'
+		'User-Agent': 'PC_UA',
+		'Referer': 'http://www.xb6v.com/'
 	},
 	timeout:5000,
-	class_parse:'#menus&&li:gt(1);a&&Text;a&&href;.*/(.*)/',
-	cate_exclude:'欧美剧|旧版6v',
+	class_name:'最新50部&喜剧片&动作片&爱情片&科幻片&恐怖片&剧情片&战争片&纪录片&动画片&电视剧&综艺',
+	class_url:'qian50m.html&xijupian&dongzuopian&aiqingpian&kehuanpian&kongbupian&juqingpian&zhanzhengpian&jilupian&donghuapian&dianshiju&ZongYi',
 	play_parse:true,
+	play_json:[{
+		re:'*',
+		json:{
+			parse:0,
+			jx:0
+		}
+	}],
+	lazy:'',
 	limit:6,
-	推荐: '*',
-	一级: '#post_container&&li;h2&&Text;img&&src;.info_date&&Text;a&&href',
-	二级: {
-		"title": "#content&&h1&&Text;.info_category&&Text",
-		"img": "#post_content&&img&&src",
-		"desc": ";;;#post_content&&p:eq(0)&&Text;#post_content&&p:eq(2)&&Text",
-		"content": "#post_content&&p:eq(1)&&Text",
-		"tabs": `js:
-			TABS = ["LEKBOX磁力"];
-			let tabs = pdfa(html, '#content&&h3:not(:contains(网盘))');
-			tabs.forEach((it) => {
-				TABS.push(pdfh(it, "body&&Text").replace('播放地址','LEKBOX在线').replace('（无插件 极速播放）','一').replace('（无需安装插件）','二'))
+	推荐:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+let d = [];
+let html = request(input);
+let list = pdfa(html, 'div.mainleft ul#post_container li');
+list.forEach(it => {
+	d.push({
+		title: pdfh(it, 'div.thumbnail img&&alt'),
+		desc: pdfh(it, 'div.info&&span.info_date&&Text') + ' / ' + pdfh(it, 'div.info&&span.info_category&&Text'),
+		pic_url: pd(it, 'div.thumbnail img&&src', HOST),
+		url: pdfh(it, 'div.thumbnail&&a&&href')
+	});
+});
+setResult(d);
+	`,
+	一级:'',
+	一级:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+let d = [];
+if (MY_CATE !== 'qian50m.html') {
+	let turl = (MY_PAGE === 1)? '/' : '/index_'+ MY_PAGE + '.html';
+	input = rule.homeUrl + MY_CATE + turl;
+	let html = request(input);
+	let list = pdfa(html, 'div.mainleft ul#post_container li');
+	list.forEach(it => {
+		d.push({
+			title: pdfh(it, 'div.thumbnail img&&alt'),
+			desc: pdfh(it, 'div.info&&span.info_date&&Text') + ' / ' + pdfh(it, 'div.info&&span.info_category&&Text'),
+			pic_url: pd(it, 'div.thumbnail img&&src', HOST),
+			url: pdfh(it, 'div.thumbnail&&a&&href')
+		});
+	})
+}else{
+	input = rule.homeUrl + MY_CATE;
+	let html = request(input);
+	let list = pdfa(html, 'div.container div#tab-content&&ul&&li');
+	list.forEach(it => {
+		let title = pdfh(it, 'a&&Text');
+		if (title!==""){
+			d.push({
+				title: title,
+				desc: pdfh(it, 'a&&Text'),
+				pic_url: '',
+				url: pdfh(it, 'a&&href')
 			});
-		`,
-		"lists": `js:
-			log(TABS);
-			pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-			LISTS = [];
-			let i = 1;
-			TABS.forEach(function(tab) {
-				if (/LEKBOX磁力/.test(tab)) {
-					var d = pdfa(html, '.context&&td');
-					d = d.map(function(it) {
-						var title = pdfh(it, 'a&&Text');
-						var burl = pd(it, 'a&&href');
-						return title + '$' + burl
-					});
-					LISTS.push(d)
-				} else if (/LEKBOX在线/.test(tab) && i <= TABS.length-1) {
-					var d = pdfa(html, '.context&&.widget:eq(list_idx)&&a'.replace("list_idx", i));
-					d = d.map(function(it) {
-						var title = pdfh(it, 'a&&Text');
-						var burl = pd(it, 'a&&href');
-						return title + '$' + burl
-					});
-					LISTS.push(d)
-					i = i + 1;
-				}
-			});
-		`,
+		}
+	})
+}
+setResult(d);
+`,
+	二级:{
+		title:"div.article_container h1&&Text",
+		img:"div#post_content img&&src",
+		desc:"div#post_content&&Text",
+		content:"div#post_content&&Text",
+		tabs:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+TABS=[]
+let d = pdfa(html, 'div#post_content table tbody tr a');
+let tabsa = [];
+let tabsq = [];
+let tabsm = false;
+let tabse = false;
+let tabm3u8 = [];
+d.forEach(function(it) {
+	let burl = pdfh(it, 'a&&href');
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		tabsa.push("阿里雲盤");
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		tabsq.push("夸克網盤");
+	}else if (burl.startsWith("magnet")){
+		tabsm = true;
+	}else if (burl.startsWith("ed2k")){
+		tabse = true;
+	}
+});
+if (false){
+d = pdfa(html, 'div:has(>div#post_content) div.widget:has(>h3)');
+d.forEach(function(it) {
+	tabm3u8.push(pdfh(it, 'h3&&Text'));
+});
+}
+if (tabsm === true){
+	TABS.push("磁力");
+}
+if (tabse === true){
+	TABS.push("電驢");
+}
+if (false && tabsa.length + tabsq.length > 1){
+	TABS.push("選擇右側綫路");
+}
+let tmpIndex;
+tmpIndex=1;
+tabsa.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tmpIndex=1;
+tabsq.forEach(function(it){
+	TABS.push(it + tmpIndex);
+	tmpIndex = tmpIndex + 1;
+});
+tabm3u8.forEach(function(it){
+	TABS.push(it);
+});
+log('xb6v TABS >>>>>>>>>>>>>>>>>>' + TABS);
+`,
+		lists:`js:
+log(TABS);
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+LISTS = [];
+let d = pdfa(html, 'div#post_content table tbody tr a');
+let lista = [];
+let listq = [];
+let listm = [];
+let liste = [];
+let listm3u8 = {};
+d.forEach(function(it){
+	let burl = pdfh(it, 'a&&href');
+	let title = pdfh(it, 'a&&Text');
+	log('xb6v title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
+	log('xb6v burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
+	let loopresult = title + '$' + burl;
+	if (burl.startsWith("https://www.aliyundrive.com/s/") || burl.startsWith("https://www.alipan.com/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=ali&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+                        burl = "push://" + burl;
+                }
+		loopresult = title + '$' + burl;
+		lista.push(loopresult);
+	}else if (burl.startsWith("https://pan.quark.cn/s/")){
+		if (true){
+		if (TABS.length==1){
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&confirm=0&url=" + encodeURIComponent(burl);
+		}else{
+			burl = "http://127.0.0.1:9978/proxy?do=quark&type=push&url=" + encodeURIComponent(burl);
+		}
+		}else{
+                        burl = "push://" + burl;
+                }
+		loopresult = title + '$' + burl;
+		listq.push(loopresult);
+	}else if (burl.startsWith("magnet")){
+		listm.push(loopresult);
+	}else if (burl.startsWith("ed2k")){
+		liste.push(loopresult);
+	}
+});
+if (listm.length>0){
+	LISTS.push(listm);
+}
+if (liste.length>0){
+	LISTS.push(liste);
+}
+if (false && lista.length + listq.length > 1){
+	LISTS.push(["選擇右側綫路，或3秒後自動跳過$http://127.0.0.1:10079/delay/"]);
+}
+lista.forEach(function(it){
+	LISTS.push([it]);
+});
+listq.forEach(function(it){
+	LISTS.push([it]);
+});
+for ( const key in listm3u8 ){
+	if (listm3u8.hasOwnProperty(key)){
+		LISTS.push(listm3u8[key]);
+	}
+};
+`,
+
 	},
-	搜索: '*',
+	搜索:`js:
+pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+let params = 'show=title&tempid=1&tbname=article&mid=1&dopost=search&submit=&keyboard=' + encodeURIComponent(KEY);
+let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
+let postData = {
+    method: "POST",
+    body: params
+};
+delete(_fetch_params.headers['Content-Type']);
+Object.assign(_fetch_params, postData);
+log("xb6v search postData>>>>>>>>>>>>>>>" + JSON.stringify(_fetch_params));
+let search_html = request( HOST + '/e/search/index.php', _fetch_params, true);
+//log("xb6v search result>>>>>>>>>>>>>>>" + search_html);
+let d=[];
+let dlist = pdfa(search_html, 'div.mainleft&&ul#post_container&&li');
+dlist.forEach(function(it){
+	let title = pdfh(it, 'div.thumbnail img&&alt').replace( /(<([^>]+)>)/ig, '');
+	if (searchObj.quick === true){
+		if (false && title.includes(KEY)){
+			title = KEY;
+		}
+	}
+	let img = pd(it, 'div.thumbnail img&&src', HOST);
+	let content = pdfh(it, 'div.article div.entry_post&&Text');
+	let desc = pdfh(it, 'div.info&&span.info_date&&Text');
+	let url = pd(it, 'div.thumbnail&&a&&href', HOST);
+	d.push({
+		title:title,
+		img:img,
+		content:content,
+		desc:desc,
+		url:url
+		});
+});
+setResult(d);
+`,
 }
